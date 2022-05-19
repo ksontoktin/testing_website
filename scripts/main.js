f185d0e98651548a087483325f41c0cb0c2fb66e
Константин <suhovk@bk.ru>
2022-05-20 00:08:40 +0300

    some more changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,34 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/my_photo.png') {
+      myImage.setAttribute ('src','images/snejinka.jpeg');
+    } else {
+      myImage.setAttribute ('src','images/my_photo.png');
+    }
+}
+
+
+
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+
+function setUserName() {
+  var myName = prompt('как тебя зовут');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'привет ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'привет ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
