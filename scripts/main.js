3fb0b00c5fdbf7d68284b50e5b7fd8e269212b6d
Константин <suhovk@bk.ru>
2022-05-20 15:21:04 +0300

    major changes

--- scripts/main.js
+++ scripts/main.js
@@ -9,44 +9,6 @@ myImage.onclick = function() {
     }
 }
 
-
-var canvas = document.getElementById("myCanvas"), 
-    context = canvas.getContext("2d"),
-    w = canvas.width,
-    h=canvas.height;
-     
-var mouse = { x:0, y:0};
-var draw = false;
- 
-canvas.addEventListener("mousedown", function(e){
-     
-    mouse.x = e.pageX - this.offsetLeft;
-    mouse.y = e.pageY - this.offsetTop;
-    draw = true;
-    context.beginPath();
-    context.moveTo(mouse.x, mouse.y);
-});
-canvas.addEventListener("mousemove", function(e){
-     
-    if(draw==true){
-     
-        mouse.x = e.pageX - this.offsetLeft;
-        mouse.y = e.pageY - this.offsetTop;
-        context.lineTo(mouse.x, mouse.y);
-        context.stroke();
-    }
-});
-canvas.addEventListener("mouseup", function(e){
-     
-    mouse.x = e.pageX - this.offsetLeft;
-    mouse.y = e.pageY - this.offsetTop;
-    context.lineTo(mouse.x, mouse.y);
-    context.stroke();
-    context.closePath();
-    draw = false;
-});
-
-
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
---
+++ scripts/main2.js
@@ -0,0 +1,35 @@
+var canvas = document.getElementById("myCanvas"), 
+    context = canvas.getContext("2d"),
+    w = canvas.width,
+    h=canvas.height;
+     
+var mouse = { x:0, y:0};
+var draw = false;
+ 
+canvas.addEventListener("mousedown", function(e){
+     
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    draw = true;
+    context.beginPath();
+    context.moveTo(mouse.x, mouse.y);
+});
+canvas.addEventListener("mousemove", function(e){
+     
+    if(draw==true){
+     
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+        context.lineTo(mouse.x, mouse.y);
+        context.stroke();
+    }
+});
+canvas.addEventListener("mouseup", function(e){
+     
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    context.lineTo(mouse.x, mouse.y);
+    context.stroke();
+    context.closePath();
+    draw = false;
+});
